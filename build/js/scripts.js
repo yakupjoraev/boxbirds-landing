b10409f17813dbcb77e693c7cc2e0e43bb0d65f0
Yakup <yakupjoraev@gmail.com>
2023-05-01 17:10:24 +0300

    added 2 modal windows

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -188,4 +188,46 @@ function modal() {
 
 }
 
-modal()
+modal();
+
+
+
+function passwordSee() {
+  const passwordGroups = document.querySelectorAll('.form-group input[type="password"]');
+
+  passwordGroups.forEach(passwordGroup => {
+    const passwordInput = passwordGroup;
+    const eyeButton = passwordGroup.parentNode.querySelector('.form-eye');
+    const eyeImage = eyeButton.querySelector('img');
+
+    eyeButton.addEventListener('click', function () {
+      if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        eyeImage.src = './img/eye-open.svg';
+      } else {
+        passwordInput.type = 'password';
+        eyeImage.src = './img/eye-close.svg';
+      }
+    });
+  });
+
+
+}
+
+passwordSee();
+
+function checked() {
+  const signUpForm = document.querySelector('.signup-form')
+  const checkbox = signUpForm.querySelector('.modal__form-checkbox');
+  const button = signUpForm.querySelector('.form-btn');
+
+  checkbox.addEventListener('change', function () {
+    if (this.checked) {
+      button.removeAttribute('disabled');
+    } else {
+      button.setAttribute('disabled', '');
+    }
+  });
+}
+
+checked();
